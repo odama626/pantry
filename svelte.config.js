5690f67dca0d750d05e3c8fe392ea5159d917997
Adam Sparks <adamsparks92@gmail.com>
2023-01-14 21:29:24 -0800

    use oder version

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-import vercel from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,7 +8,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: vercel()
+		adapter: adapter()
 	}
 };
 
