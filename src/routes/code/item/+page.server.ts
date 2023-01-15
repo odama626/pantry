3057b055c7ea05c64505607f48d6f020dbbfc4f7
Adam Sparks <adamsparks92@gmail.com>
2023-01-14 21:48:33 -0800

    fix id bug

--- src/routes/code/item/+page.server.ts
+++ src/routes/code/item/+page.server.ts
@@ -1,36 +1,42 @@
 import { airtable } from '$lib/server/db';
-import { redirect } from '@sveltejs/kit';
+import { fail, redirect } from '@sveltejs/kit';
 import type { Actions } from './$types';
 
 export const actions: Actions = {
 	edit: async ({ request }) => {
 		const data = await request.formData();
 
-		console.log({ code: data.get('code') });
+		try {
+			console.log({ code: data.get('code') });
 
-		const fields = {
-			code: data.get('code'),
-			description: data.get('description'),
-			'stored at': data.get('stored at')
-		};
+			const fields = {
+				code: data.get('code'),
+				description: data.get('description'),
+				'stored at': data.get('stored at')
+			};
 
-		if (data.get('id')?.length) {
-			airtable('items').update([
-				{
-					id: data.get('id'),
-					fields
-				}
-			]);
-		} else {
-			airtable('items').create([
-				{
-					fields
-				}
-			]);
+			const id = data.get('id');
+
+			console.log({ id });
+
+			if (id?.length) {
+				await airtable('items').update([
+					{
+						id: data.get('id'),
+						fields
+					}
+				]);
+			} else {
+				await airtable('items').create([
+					{
+						fields
+					}
+				]);
+			}
+		} catch (e) {
+			console.error(e);
+			return fail(500, e.message);
 		}
-
 		throw redirect(303, `/code/item/${data.get('code')}`);
-
-		return { success: true };
 	}
 };
--- src/routes/code/item/[code]/edit/+page.svelte
+++ src/routes/code/item/[code]/edit/+page.svelte
@@ -1,18 +1,19 @@
 <script lang="ts">
-  import type { PageData } from './$types';
+	import type { PageData } from './$types';
 
-  export let data: PageData;
+	export let data: PageData;
 
-  console.log({data})
+	console.log({ data });
+	const today = new Date().toJSON().split('T')[0];
 </script>
 
 <main class="container">
-  <h2>Edit</h2>
-  <form method='post' action='/code/item?/edit'>
-  <input type='hidden' name='id' value={data.id} />
-  <input type='hidden' name='code' value={data.fields.code} />
-  <input name='description' value={data.fields.description ?? ''} />
-  <input name='stored at'  type='date' value={data.fields['stored at']} />
-  <button>Save</button>
-</form>
+	<h2>Edit</h2>
+	<form method="post" action="/code/item?/edit">
+		<input type="hidden" name="id" value={data.id ?? null} />
+		<input type="hidden" name="code" value={data.fields.code} />
+		<input name="description" value={data.fields.description ?? ''} />
+		<input name="stored at" type="date" value={data.fields['stored at'] ?? today} />
+		<button>Save</button>
+	</form>
 </main>
