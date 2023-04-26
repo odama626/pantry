785d586ea217b14130621b78872cbbfa2d79a598
Adam Sparks <adamsparks92@gmail.com>
2023-04-26 00:42:54 -0700

    fix colorHash error

--- src/lib/hashColor.ts
+++ src/lib/hashColor.ts
@@ -1,11 +1,9 @@
 import ColorHash from 'color-hash';
 
-
-const color = new ColorHash({
-  saturation: [0.35],
-  lightness: [0.35]
-});
-
-export default function hashFunction(text: string):string {
-  return color.hex(text);
+export default function hashFunction(text: string): string {
+	const color = new ColorHash({
+		saturation: [0.35],
+		lightness: [0.35]
+	});
+	return color.hex(text);
 }
