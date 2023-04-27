2799017d2ebd84d2fbb6d643292d889d7f49f6d6
Adam Sparks <adamsparks92@gmail.com>
2023-04-26 22:49:30 -0700

    fix color-hash bundle

--- src/lib/hashColor.ts
+++ src/lib/hashColor.ts
@@ -1,9 +1,10 @@
-import ColorHash from 'color-hash';
+import ColorHash from 'color-hash/dist/bundle';
 
 export default function hashFunction(text: string): string {
 	const color = new ColorHash({
 		saturation: [0.35],
-		lightness: [0.35]
+		lightness: [0.35],
+		hash: 'bkdr'
 	});
 	return color.hex(text);
 }
