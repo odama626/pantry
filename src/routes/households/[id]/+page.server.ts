f37564a6e01fa0f6b50ba8b17dd7ef2913d8c0f2
Adam Sparks <adamsparks92@gmail.com>
2023-05-12 23:42:52 -0700

    household image

--- src/routes/households/[id]/+page.server.ts
+++ src/routes/households/[id]/+page.server.ts
@@ -9,11 +9,15 @@ export async function load({ params, locals }) {
     const rawHousehold = await pb.collection('households').getOne<HouseholdsRecord>(id, { expand: 'users' }).catch(() => { });
     const rawMembers = await pb.collection('users').getFullList<UsersResponse>({ filter: `households ~ '${rawHousehold.id}'` });
 
-    // if (!rawHousehold.image) {
-    //   rawHousehold.image = await getIcon(rawHousehold.name);
-    //   pb.collection('households').update(rawHousehold.id, rawHousehold);
-    // }
-
+    if (!rawHousehold.image) {
+      rawHousehold.image = await getIcon(rawHousehold.name);
+      const formData = new FormData();
+      formData.set('image', await fetch(rawHousehold.image).then(r => r.blob()));
+      pb.collection('households').update(rawHousehold.id, formData);
+      
+    } else {
+      rawHousehold.image = pb.getFileUrl(rawHousehold, rawHousehold.image);
+    }
 
     const household = rawHousehold && (exportRecord(rawHousehold));
     const members = rawMembers && exportRecord(rawMembers);
